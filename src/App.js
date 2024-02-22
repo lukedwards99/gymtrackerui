dbabeaa888ce018004e474f5b6ffc6f887d95df7
lukedwards99 <luke.edwards20@gmail.com>
2024-02-21 20:00:50 -0600

    basic setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
