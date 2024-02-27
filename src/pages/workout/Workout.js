bdf1b54f99f12804ed740e7d6535e90155059809
lukedwards99 <luke.edwards20@gmail.com>
2024-02-26 18:19:59 -0600

    setup workout data

--- src/pages/workout/Workout.js
+++ src/pages/workout/Workout.js
@@ -1,7 +1,29 @@
-import {useParams} from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+let workoutIdParam = null
 
-export default function Workout(){
-    const {id} = useParams();
+export default function Workout() {
+    const { id: workoutId } = useParams();
+    console.log(workoutId)
+    workoutIdParam = workoutId
+    const { data, error, isLoading, refetch } = useQuery({ queryKey: ['workout_overview'], queryFn: getWorkoutById })
 
-    return <h1>Workout: {id}</h1>
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>An error occurred: {error.message}</div>;
+
+    const workoutDate = data.
+
+    return (
+        <div className="container">
+            <h1>Workout: {workoutId}</h1>
+            <p>{JSON.stringify(data)}</p>
+        </div>
+    )
+}
+
+async function getWorkoutById() {
+    const { data } = await axios.get(`http://localhost:3030/workout/${workoutIdParam}`);
+    //console.log("workouts: " + JSON.stringify(data))
+    return data;
 }
